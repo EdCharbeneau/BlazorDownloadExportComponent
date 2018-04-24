fbf99482db48bd0f254a3bc8a5ea17d764d1eac1
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-04-23 20:31:55 -0400

    Added interop to fix issue with download not working in Edge/IE. Thx @Suchiman for bringing up the issue.

---
+++ DownloadComponents/content/MsSaveBlobInterop.js
@@ -0,0 +1,20 @@
+﻿Blazor.registerFunction('DownloadComponents.MsSaveBlobInterop.MsSaveBlob', function (payload, filename) {
+
+    var blob = new Blob([payload], { type: "text/csv;charset=utf-8;" });
+
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename)
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style = "visibility:hidden";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+})
